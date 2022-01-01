bba571b2ef25609ddc57cbb1ef084980084175c0
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-01-01 10:10:27 -0300

    adicionandol foto

--- backend/urls.py
+++ backend/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
 from livros.api import viewsets as livrosviewset
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 route = routers.DefaultRouter() #rota principal default
 route.register(r'livros/', livrosviewset.LivrosViewset, basename='Livros')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- livros/models.py
+++ livros/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Livros(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField()
     author = models.CharField(max_length=200)
     publishing_company =models.CharField(max_length=200)
-    pages = models.IntegerField()
+    pages = models.IntegerField(upload_to='fotos/%d/%m/%Y', blank=True)
     
